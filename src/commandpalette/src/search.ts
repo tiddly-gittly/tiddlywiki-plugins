e0810fdf2d0d1422bcd05ee8448f2ea8a3be6c8c
Lin Onetwo <linonetwo012@gmail.com>
2021-10-04 01:59:48 +0800

    feat: add typing to CommandPaletteWidget

---
+++ src/commandpalette/src/search.ts
@@ -0,0 +1,20 @@
+import Fuse from 'fuse.js';
+import pinyin from 'pinyin';
+import type { IResult } from './CommandPaletteWidget';
+
+export function hasPinyinMatchOrFuseMatch(userInput: string, results: IResult[]) {
+  const fuse = new Fuse(results, {
+    keys: ['name', 'caption', 'hint'],
+    getFn: (object, keyPath): string => {
+      if (Array.isArray(keyPath)) {
+        // we don't have nested keys
+        return '';
+      } else {
+        const value = object[keyPath as keyof IResult] ?? '';
+        return `${value}${pinyin(value, { style: pinyin.STYLE_NORMAL }).join('')}`;
+      }
+    },
+    ignoreLocation: true,
+  });
+  return fuse.search(userInput);
+}
