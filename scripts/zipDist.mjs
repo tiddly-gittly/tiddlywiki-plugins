7e23c76b7bfc4cbc8b83986236e7925392cd2f1a
林东吴 <lindongwu@bytedance.com>
2021-10-07 19:21:36 +0800

    chore: zip dist

---
+++ scripts/zipDist.mjs
@@ -0,0 +1,23 @@
+import archiver from 'archiver';
+
+/**
+ * @param {String} source
+ * @param {String} out
+ * @returns {Promise}
+ */
+function zipDirectory(source, out) {
+  const archive = archiver('zip', { zlib: { level: 9 } });
+  const stream = fs.createWriteStream(out);
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(source, false)
+      .on('error', (err) => reject(err))
+      .pipe(stream);
+
+    stream.on('close', () => resolve());
+    archive.finalize();
+  });
+}
+
+await zipDirectory(path.join(__dirname, '..', 'dist', 'linonetwo'), path.join(__dirname, '..', 'dist', 'linonetwo.zip'));
