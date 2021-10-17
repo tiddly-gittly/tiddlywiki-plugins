31270823e0e13f52a9dc82984d53c7722a581634
Lin Onetwo <linonetwo012@gmail.com>
2021-10-17 21:24:18 +0800

    fix: linux cp command is buggy

--- src/pinyin-fuzzy-search/scripts/after-build.mjs
+++ src/pinyin-fuzzy-search/scripts/after-build.mjs
@@ -1,3 +1,4 @@
 const repoDir = path.join(__dirname, '..');
 const distDir = path.join(repoDir, '..', '..', 'dist', 'plugins', 'linonetwo', 'pinyin-fuzzy-search');
-await $`cp -r ${repoDir}/src/ ${distDir}/`;
+// cross platform cp -r ${repoDir}/src/ ${distDir}/
+fs.copy(path.join(repoDir, 'src'), distDir)
