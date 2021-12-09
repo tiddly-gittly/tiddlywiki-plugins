2c2199c25caca9ffb90c808715cfd55e0a34663d
Lin Onetwo <linonetwo012@gmail.com>
2021-12-10 00:14:27 +0800

    feat: build json plugins

---
+++ scripts/buildJSONPlugins.mjs
@@ -0,0 +1,10 @@
+await $`
+cd ${path.join(__dirname, '..', 'dist')} && \
+npx cross-env \
+    TIDDLYWIKI_PLUGIN_PATH='./plugins' \
+    TIDDLYWIKI_THEME_PATH='./themes' \
+    TIDDLYWIKI_LANGUAGE_PATH='./languages' \
+    npx tiddlywiki . \
+    --makelibrary $:/UpgradeLibrary \
+    --savelibrarytiddlers $:/UpgradeLibrary '[prefix[$:/plugins/linonetwo/]]' ./
+`;
--- scripts/buildPlugins.mjs
+++ scripts/buildPlugins.mjs
@@ -1,4 +1,4 @@
-const ignored = ['watch-fs'];
+const ignored = ['watch-fs', 'google-calendar-import'];
 
 const folders = fs.readdirSync('./src').filter((item) => !ignored.includes(item));
 await Promise.all(folders.filter((pathName) => !pathName.startsWith('.')).map((folder) => $`cd ${path.join(__dirname, '..', 'src', folder)} && npm run build`));
