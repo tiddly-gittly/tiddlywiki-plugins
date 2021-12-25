36a3443c0200394e2c65a6640c43db41e29a219a
Lin Onetwo <linonetwo012@gmail.com>
2021-12-26 02:28:13 +0800

    fix: startup module usage

--- plugins/linonetwo/open-in-external-app/listener.js
+++ plugins/linonetwo/open-in-external-app/listener.js
@@ -1,17 +1,19 @@
 // Listen for the tm-home message
-if ($tw.browser) {
-  $tw.rootWidget.addEventListener('tm-open-in-external-app', async function (event) {
-    let title = '';
-    if (typeof event.param === 'string') {
-      // for button usage
-      title = event.param;
-    } else if (typeof event.paramObject === 'object') {
-      // Get the specified additional fields, for message usage
-      additionalFields = event.paramObject;
-      if (additionalFields && additionalFields.title) {
-        title = additionalFields.title;
+exports.startup = function () {
+  if ($tw.browser) {
+    $tw.rootWidget.addEventListener('tm-open-in-external-app', async function (event) {
+      let title = '';
+      if (typeof event.param === 'string') {
+        // for button usage
+        title = event.param;
+      } else if (typeof event.paramObject === 'object') {
+        // Get the specified additional fields, for message usage
+        additionalFields = event.paramObject;
+        if (additionalFields && additionalFields.title) {
+          title = additionalFields.title;
+        }
       }
-    }
-    await window.service.wiki.openTiddlerInExternal();
-  });
-}
+      await window.service.wiki.openTiddlerInExternal(title);
+    });
+  }
+};
