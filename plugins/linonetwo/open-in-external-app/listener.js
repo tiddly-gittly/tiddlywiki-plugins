b10fa10872d6581185199d3217bfc586420d8d57
Lin Onetwo <linonetwo012@gmail.com>
2021-12-25 20:03:46 +0800

    feat: basic open external plugin

---
+++ plugins/linonetwo/open-in-external-app/listener.js
@@ -0,0 +1,15 @@
+// Listen for the tm-home message
+$tw.rootWidget.addEventListener('tm-open-in-external-app', async function (event) {
+  let title = '';
+  if (typeof event.param === 'string') {
+    // for button usage
+    title = event.param;
+  } else if (typeof event.paramObject === 'object') {
+    // Get the specified additional fields, for message usage
+    additionalFields = event.paramObject;
+    if (additionalFields && additionalFields.title) {
+      title = additionalFields.title;
+    }
+  }
+  await window.service.wiki.openTiddlerInExternal();
+});
