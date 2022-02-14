a7b78047edf91de7a5159478be9ea1ffca7877d6
Lin Onetwo <linonetwo012@gmail.com>
2022-02-14 13:32:12 +0800

    chore: 都装上 tw5-typed

--- src/smart-field/src/schema.ts
+++ src/smart-field/src/schema.ts
@@ -1,4 +1,4 @@
-import { SourceIterator, IFilterOperatorParamOperator } from "tiddlywiki";
+import { SourceIterator, IFilterOperatorParamOperator } from 'tiddlywiki';
 
 /**
  *
@@ -8,41 +8,4 @@ import { SourceIterator, IFilterOperatorParamOperator } from "tiddlywiki";
  * @param options
  * @returns
  */
-export const getFormTagJSONSchema = (source: (iter: SourceIterator) => void, operator: IFilterOperatorParamOperator) => {
-  const invert = operator.prefix === '!';
-  if (operator.suffixes) {
-    let hasFlag = function (flag: string) {
-      return (operator.suffixes?.[1] ?? []).indexOf(flag) !== -1;
-    };
-    let excludeFields = false;
-    let fieldList = operator.suffixes[0] || [];
-    let firstField = fieldList[0] || '';
-    let firstChar = firstField.charAt(0);
-    let fields: string[];
-    if (firstChar === '-') {
-      fields = [firstField.slice(1)].concat(fieldList.slice(1));
-      excludeFields = true;
-    } else if (fieldList[0] === '*') {
-      fields = [];
-      excludeFields = true;
-    } else {
-      fields = fieldList.slice(0);
-    }
-    return fuzzySearchWiki(operator.operand, {
-      source: source,
-      invert: invert,
-      field: fields,
-      excludeField: excludeFields,
-      caseSensitive: hasFlag('casesensitive'),
-      literal: hasFlag('literal'),
-      whitespace: hasFlag('whitespace'),
-      anchored: hasFlag('anchored'),
-      words: hasFlag('words'),
-    });
-  } else {
-    return fuzzySearchWiki(operator.operand, {
-      source: source,
-      invert: invert,
-    });
-  }
-};
+export const getFormTagJSONSchema = (source: (iter: SourceIterator) => void, operator: IFilterOperatorParamOperator) => {};
