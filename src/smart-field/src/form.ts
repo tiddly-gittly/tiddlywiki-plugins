83a0e0efc5b3703a5241c2a53356723f50ac027a
Lin Onetwo <linonetwo012@gmail.com>
2022-02-15 01:21:36 +0800

    feat: get schema for widget

--- src/smart-field/src/form.ts
+++ src/smart-field/src/form.ts
@@ -1,5 +1,6 @@
 import { withTheme } from '@rjsf/core';
 import { Theme as Mui5Theme } from '@rjsf/material-ui';
+import type { JSONSchema6 } from 'json-schema';
 
 // Make modifications to the theme with your own fields and widgets
 
@@ -7,25 +8,67 @@ const Form = withTheme(Mui5Theme);
 
 const Widget = require('$:/plugins/linonetwo/tw-react/widget.js').widget;
 
-const schema = {
-  title: 'Todo',
-  type: 'object',
-  required: ['title'],
-  properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false },
-  },
-};
-
 const log = (type: string) => console.log.bind(console, type);
 
 class FormWidget extends Widget {
   reactComponent = Form;
-  getProps = () => ({
-    schema: schema,
-    onChange: log('changed'),
-    onSubmit: log('submitted'),
-    onError: log('errors'),
-  });
+  getProps = () => {
+    /** Found "form tags" of the current tiddler, we will read their fields to get fields that will show up in our user's tiddler. */
+    const formTagsFilter: string = `[<currentTiddler>tagging[]tagged[$:/tags/Ontology/Form]]`;
+    const formTags: string[] = $tw.wiki.compileFilter(formTagsFilter)();
+    // prepare ontology names we need to find
+    /**
+     * Fields that will show in user's tiddler, and each field's ontology name.
+     */
+    const formFieldsAndOntologies: { field: string; ontology: string }[] = formTags.flatMap((tiddlerTitle) => {
+      const fields = $tw.wiki.getTiddler(tiddlerTitle)?.fields;
+      if (!fields) {
+        return [];
+      }
+      /** We will look for fields like `properties.xxx` */
+      return Object.keys(fields)
+        .filter((fieldName) => fieldName.startsWith('properties'))
+        .map((fieldName) => {
+          const ontologyName = fields[fieldName];
+          const finalFieldName = fieldName.replace('properties.', '');
+          return { field: finalFieldName, ontology: ontologyName };
+        });
+    });
+    // get JSONSchema from the ontology, using ontologyName.fieldName
+    const schema: JSONSchema6 = {
+      title: 'Example',
+      type: 'object',
+      required: [],
+      properties: {
+        // title: { type: 'string', title: 'Title', default: 'A new task' },
+        // done: { type: 'boolean', title: 'Done?', default: false },
+      },
+    };
+    formFieldsAndOntologies.forEach(({ field, ontology }) => {
+      /**
+       * A tiddler tagged with `$:/tags/Ontology/Field`
+       */
+      const ontologyFieldTiddler = $tw.wiki.getTiddler(`$:/tags/Ontology/${ontology}`)?.fields;
+      if (!ontologyFieldTiddler?.tags?.includes('$:/tags/Ontology/Field')) {
+        return;
+      }
+      /**
+       * JSONSchema in tiddler's text field
+       */
+      const schemaFragmentOfThisField: string = ontologyFieldTiddler.text;
+      try {
+        const parsedSchema = JSON.parse(schemaFragmentOfThisField);
+        schema.properties![field] = parsedSchema;
+      } catch {
+        // TODO: handle error and warn user about a bad form plugin installed
+      }
+    });
+    return {
+      schema: schema,
+      onChange: log('changed'),
+      onSubmit: log('submitted'),
+      onError: log('errors'),
+    };
+  };
 }
 exports.formWidget = FormWidget;
