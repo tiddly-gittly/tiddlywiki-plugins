4ee0dd6e103822dd74675f945c7f5f7d40fecf7b
Lin Onetwo <linonetwo012@gmail.com>
2022-10-23 15:32:49 +0800

    fix: npm i needs --legacy-peer-deps

--- scripts/buildPlugins.mjs
+++ scripts/buildPlugins.mjs
@@ -1,4 +1,4 @@
 const ignored = ['watch-fs', 'google-calendar-import'];
 
 const folders = fs.readdirSync('./src').filter((item) => !ignored.includes(item));
-await Promise.all(folders.filter((pathName) => !pathName.startsWith('.')).map((folder) => $`cd ${path.join(__dirname, '..', 'src', folder)} && npm i && npm run build`));
+await Promise.all(folders.filter((pathName) => !pathName.startsWith('.')).map((folder) => $`cd ${path.join(__dirname, '..', 'src', folder)} && npm i --legacy-peer-deps && npm run build`));
