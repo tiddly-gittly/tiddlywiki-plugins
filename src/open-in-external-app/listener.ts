121f0c6ba69e8fe83475af7d920b30c91ae5b5bd
lin onetwo <linonetwo012@gmail.com>
2024-01-06 23:48:11 +0800

    feat: update tidgi api used in open-in-external-app

--- src/open-in-external-app/listener.ts
+++ src/open-in-external-app/listener.ts
@@ -1,30 +1,63 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions, no-var */
+import { IWidgetEvent } from 'tiddlywiki';
+
+declare var exports: {
+  startup: () => void;
+};
+declare global {
+  interface Window {
+    // methods copy from TidGi-Desktop, should update if it changes
+    service: {
+      native: {
+        openPath(filePath: string, showItemInFolder?: boolean): Promise<void>;
+      };
+      wiki: {
+        getTiddlerFilePath(title: string, workspaceID?: string): Promise<string | undefined>;
+      };
+    };
+  }
+}
+
 // Listen for the tm-home message
-exports.startup = function () {
-  if ($tw.browser) {
-    $tw.rootWidget.addEventListener('tm-open-in-external-app', async function (event) {
-      let title = '';
-      if (typeof event.param === 'string') {
-        // for button usage
-        title = event.param;
-      } else if (typeof event.paramObject === 'object') {
-        // Get the specified additional fields, for message usage
-        additionalFields = event.paramObject;
-        if (additionalFields && additionalFields.title) {
-          title = additionalFields.title;
-        }
+exports.startup = () => {
+  async function getFilePath(event: IWidgetEvent): Promise<string | undefined> {
+    let title = '';
+    if (typeof event.param === 'string') {
+      // for button usage
+      title = event.param;
+    } else if (typeof event.paramObject === 'object') {
+      // Get the specified additional fields, for message usage
+      const additionalFields = event.paramObject as { title?: string };
+      if (additionalFields?.title) {
+        title = additionalFields.title;
       }
-      await window?.service?.wiki?.openTiddlerInExternal?.(title);
+    }
+    const filePath = await window?.service?.wiki?.getTiddlerFilePath?.(title);
+    return filePath;
+  }
+  if ($tw.browser) {
+    $tw.rootWidget.addEventListener('tm-open-in-external-app', async (event) => {
+      const filePath = await getFilePath(event);
+      if (filePath !== undefined) {
+        await window?.service?.native?.openPath?.(filePath);
+      }
+    });
+    $tw.rootWidget.addEventListener('tm-open-in-folder', async (event) => {
+      const filePath = await getFilePath(event);
+      if (filePath !== undefined) {
+        await window?.service?.native?.openPath?.(filePath, true);
+      }
     });
 
-    $tw.rootWidget.addEventListener('tm-open-path', async function (event) {
+    $tw.rootWidget.addEventListener('tm-open-path', async (event) => {
       let filePath = '';
       if (typeof event.param === 'string') {
         // for button usage
         filePath = event.param;
       } else if (typeof event.paramObject === 'object') {
         // Get the specified additional fields, for message usage
-        additionalFields = event.paramObject;
-        if (additionalFields && additionalFields.filePath) {
+        const additionalFields = event.paramObject as { filePath?: string };
+        if (additionalFields?.filePath) {
           filePath = additionalFields.filePath;
         }
       }
